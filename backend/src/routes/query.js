b7e562fa5158db928487c80229ecb0ce51c436de
ramkrushnaw04 <ramkrushnaw04@gmail.com>
2025-05-14 19:06:11 +0530

    added env file to backend

--- backend/src/routes/query.js
+++ backend/src/routes/query.js
@@ -1,6 +1,9 @@
 const { GoogleGenAI } = require("@google/genai")
+require("dotenv").config();
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyD1vcwLjnr8wkPT3LXDTahJytav9sG5zIo" });
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY  });
+
+console.log(process.env.GOOGLE_API_KEY);
 
 async function query(req, res) {
     const prompt = "You are a helpful dictionary. You will be given a word and you will provide the definition of that word. You will not provide any other information or context. You will only respond with the definition only. If there are multiple meanings then you will list all of them one by one seperated by ','. Do not include any additional text or explanations. Give the meaning of the word: " + req.body.word;
